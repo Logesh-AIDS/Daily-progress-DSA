c6addbe4c720bd80d1621da46e637d7f14c28ea7
logeshalavandhan <logeshalavandhan@gmail.com>
2025-12-16 15:43:54 +0530

    Kth largest element

--- heaps/Kth largest element in an array.java
+++ heaps/Kth largest element in an array.java
@@ -1,5 +1,16 @@
 package heaps;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
-public class Kth largest element in an array {
-    
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+        }
+        for(int i=1;i<=k-1;i++){
+            pq.poll();
+        }
+        return pq.peek();
+    }
 }
